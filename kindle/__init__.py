90e40b6ca2839a0b40c3fa05ec76e65e223f4fab
Jongkuk Lim <lim.jeikei@gmail.com>
2021-02-15 21:11:58 +0900

    Update README.md and test_model_parser.py (#6)

--- kindle/__init__.py
+++ kindle/__init__.py
@@ -4,8 +4,8 @@
 - Contact: lim.jeikei@gmail.com
 """
 
-from kindle import modules
+from kindle import generator, modules
 from kindle.model import Model
 from kindle.trainer import TorchTrainer
 
-__all__ = ["modules", "Model", "TorchTrainer"]
+__all__ = ["modules", "generator", "Model", "TorchTrainer"]
--- tests/test_model_parser.py
+++ tests/test_model_parser.py
@@ -9,6 +9,7 @@ import os
 import torch
 
 from kindle.model import Model
+from kindle.torch_utils import count_model_params
 
 
 class TestModelParser:
@@ -22,13 +23,16 @@ class TestModelParser:
         """Test show case model."""
         model = Model(os.path.join("tests", "test_configs", "show_case.yaml"))
         assert model(TestModelParser.INPUT).shape == torch.Size([1, 10])
+        assert count_model_params(model) == 223154
 
     def test_vgg(self):
         """Test vgg model."""
         model = Model(os.path.join("tests", "test_configs", "vgg.yaml"))
         assert model(TestModelParser.INPUT).shape == torch.Size([1, 10])
+        assert count_model_params(model) == 3732970
 
     def test_example(self):
         """Test example model."""
         model = Model(os.path.join("tests", "test_configs", "example.yaml"))
         assert model(TestModelParser.INPUT).shape == torch.Size([1, 10])
+        assert count_model_params(model) == 137862
