afca2eb95f0f64e83b941ee28978d562e82ca501
Jongkuk Lim <lim.jeikei@gmail.com>
2021-02-23 07:56:35 +0900

    Bugfix/fix add op (#19)
    * Fix Add module gradient fail issue.
    
    * Update doc.

--- tests/test_model_train.py
+++ tests/test_model_train.py
@@ -4,6 +4,7 @@
 - Contact: lim.jeikei@gmail.com
 """
 import os
+from typing import Tuple
 
 import numpy as np
 import torch
@@ -15,15 +16,8 @@ from torchvision import datasets, transforms
 from kindle import Model, TorchTrainer
 
 
-def test_model_train():
-    if torch.cuda.is_available():
-        device = torch.device("cuda:0")
-    else:
-        device = torch.device("cpu")
-
-    model = Model(os.path.join("tests", "test_configs", "example.yaml"), verbose=True)
+def prepare_cifar10():
     batch_size = 16
-    epochs = 1
 
     preprocess = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
@@ -37,22 +31,58 @@ def test_model_train():
         "./data/cifar10", train=False, download=True, transform=preprocess
     )
 
-    subset_sampler = SubsetRandomSampler(np.arange(0, len(train_dataset), 2))
+    subset_sampler = SubsetRandomSampler(np.arange(0, len(train_dataset), 10))
 
     train_loader = DataLoader(
         train_dataset, batch_size=batch_size, sampler=subset_sampler
     )
     test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
+    return train_loader, test_loader
+
+
+def get_trainer(path: str) -> Tuple[Model, TorchTrainer]:
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+
+    model = Model(path, verbose=True)
+    model.to(device)
+
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters())
 
     trainer = TorchTrainer(model, criterion, optimizer, device=device)
-    trainer.train(train_loader, n_epoch=epochs, test_dataloader=test_loader)
+
+    return model, trainer
+
+
+def test_model_example():
+    epochs = 1
+    model, trainer = get_trainer(os.path.join("tests", "test_configs", "example.yaml"))
+
+    train_loader, test_loader = prepare_cifar10()
+
+    trainer.train(train_loader, n_epoch=epochs)
     test_loss, test_accuracy = trainer.test(test_loader)
 
     print(test_loss, test_accuracy)
-    assert test_accuracy > 0.45 and test_loss < 1.5
+    assert test_accuracy > 0.35 and test_loss < 1.7
+
+
+def test_model_showcase():
+    epochs = 1
+
+    model, trainer = get_trainer(
+        os.path.join("tests", "test_configs", "show_case.yaml")
+    )
+    train_loader, test_loader = prepare_cifar10()
+    trainer.train(train_loader, n_epoch=epochs)
+    test_loss, test_accuracy = trainer.test(test_loader)
+
+    print(test_loss, test_accuracy)
+    assert test_accuracy > 0.18 and test_loss < 2.3
 
 
 def optuna_sample():
@@ -126,5 +156,6 @@ def optuna_sample():
 
 
 if __name__ == "__main__":
-    # test_model_train()
-    optuna_sample()
+    test_model_showcase()
+    test_model_example()
+    # optuna_sample()
